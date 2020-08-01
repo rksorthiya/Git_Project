c973767264fc8eae4811caa6c4e1ef39cc845d51
rksorthiya <rsorthiya200@rku.ac.in>
2020-08-01 19:51:26 +0530

    Add files

--- app/src/main/java/com/example/mygitproject/MainActivity.java
+++ app/src/main/java/com/example/mygitproject/MainActivity.java
@@ -2,14 +2,36 @@ package com.example.mygitproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+    Handler handler;
+    Runnable runnable;
+    ImageView bgimg;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getSupportActionBar().hide();
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_splash_screen);
+        bgimg = findViewById(R.id.bgimg);
+        ((View) bgimg).animate().alpha(4000).setDuration(0);
+
+        handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent dsp = new Intent(MainActivity.this,login_activity.class);
+                startActivity(dsp);
+                finish();
+            }
+        },4000);
     }
+
+
+
 }
